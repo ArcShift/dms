6b0c8518b108fb98be3da223ec8a705ca37411c0
ArcShift <ma.kaafi@yahoo.co.id>
2020-04-14 11:38:10 +0700

    fix user data

--- assets/db/users.sql
+++ assets/db/users.sql
@@ -1,2 +1,2 @@
-INSERT INTO `module` (`id_role`, `username`, `fullname`, `pass`) VALUES 
+INSERT INTO `users` (`id_role`, `username`, `fullname`, `pass`) VALUES 
 (1, 'admin', 'Super Admin', MD5('1234'));
