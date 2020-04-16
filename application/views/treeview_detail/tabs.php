4b4742b1ef3c15eb98c6d48f287d0990327a9e86
ArcShift <ma.kaafi@yahoo.co.id>
2020-04-16 11:01:58 +0700

    form1 readonly

--- application/views/treeview_detail/tabs.php
+++ application/views/treeview_detail/tabs.php
@@ -11,7 +11,7 @@ $role = $this->session->userdata['user']['role'];
     }
 </style>
 <ul>
-    <?php foreach ($data as $d) { ?>
+    <?php foreach ($data as $k => $d) { ?>
         <li id="pasal<?php echo $d['id'] ?>" class="-list-group-item list-pasal">
             <input class="desc d-none" name="desc" value="">
             <input class="filename d-none" name="desc" value="">
@@ -19,92 +19,116 @@ $role = $this->session->userdata['user']['role'];
             <span class="title"><?php echo $d['name'] ?></span>
             <?php if ($activeModule['acc_update']) { ?>
                 <?php if ($role == 'pic' || $role = 'admin') { ?>
-                    <span class="fa fa-bars text-primary" onclick="form1(this)" title="Open Form">&nbsp;</span>
+                    <span class="fa fa-bars text-primary" onclick="form1(<?php echo $k ?>)" title="Open Form">&nbsp;</span>
                 <?php } ?>
             <?php } ?>
             <ul></ul>
         </li>
     <?php } ?>
 </ul>
-        <ul class="nav nav-tabs">
-            <li class="nav-item"><a data-toggle="tab" href="#tab-pemenuhan" class="nav-link">Pemenuhan</a></li>
-            <li class="nav-item"><a data-toggle="tab" href="#tab-pasal" class="nav-link active">Pasal</a></li>
-            <li class="nav-item"><a data-toggle="tab" href="#tab-jadwal" class="nav-link">Jadwal</a></li>
-            <li class="nav-item"><a data-toggle="tab" href="#tab-penerapan" class="nav-link">Penerapan</a></li>
+<ul class="nav nav-tabs">
+    <li class="nav-item"><a data-toggle="tab" href="#tab-pemenuhan" class="nav-link">Pemenuhan</a></li>
+    <li class="nav-item"><a data-toggle="tab" href="#tab-pasal" class="nav-link active">Pasal</a></li>
+    <li class="nav-item"><a data-toggle="tab" href="#tab-jadwal" class="nav-link">Jadwal</a></li>
+    <li class="nav-item"><a data-toggle="tab" href="#tab-penerapan" class="nav-link">Penerapan</a></li>
+</ul>
+<div class="tab-content">
+    <!--PEMENUHAN-->
+    <div class="tab-pane" id="tab-pemenuhan" role="tabpanel">
+        <ul class="list-group">
         </ul>
-        <div class="tab-content">
-            <!--PEMENUHAN-->
-            <div class="tab-pane" id="tab-pemenuhan" role="tabpanel">
+    </div>
+    <!--PASAL-->
+    <div class="tab-pane active" id="tab-pasal" role="tabpanel">
+        <div class="row">
+            <div class="col-sm-6" id="treeview-list">
                 <ul class="list-group">
+                    <li id="root" class="list-group-item">
+                        <span class="-title">ROOT</span>
+                        <ul></ul>
+                    </li>
                 </ul>
             </div>
-            <!--PASAL-->
-            <div class="tab-pane active" id="tab-pasal" role="tabpanel">
-                <div class="row">
-                    <div class="col-sm-6" id="treeview-list">
-                        <ul class="list-group">
-                            <li id="root" class="list-group-item">
-                                <span class="-title">ROOT</span>
-                                <ul></ul>
-                            </li>
-                        </ul>
+            <div class="col-sm-6" id="treeview-crud">
+                <form id="form1" class="d-none">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Form 1</h4>
+                        <button class="close" type="button" aria-label="Close" onclick="closeForm()">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <div class="col-sm-6" id="treeview-crud">
-                        <form id="formDetail" class="d-none" method="post" enctype="multipart/form-data">
-                            <div class="modal-header">
-                                <h4 class="modal-title" id="form-title"></h4>
-                                <button class="close" type="button" aria-label="Close" onclick="closeForm()">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                            </div>
-                            <div class="modal-body">
-                                <div class="form-group d-none">
-                                    <label for="parent-id">ID</label>
-                                    <input class="form-control item-id" name="id" required="" readonly="">
-                                </div>
-                                <div class="form-group">
-                                    <label for="parent">Deskripsi</label>
-                                    <input class="form-control item-desc" name="desc">
-                                </div>
-                                <div class="form-group">
-                                    <label>File</label>
-                                    <input type="file" accept=".pdf" class="form-control file" name="file" placeholder="Nama">
-                                </div>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="button" class="btn btn-transition btn-outline-warning" onclick="closeForm()">Batal</button>
-                            </div>
-                        </form>
+                    <div class="modal-body">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <td>Nama</td>
+                                    <td>:</td>
+                                    <td class="item-name"></td>
+                                </tr>
+                                <tr>
+                                    <td>Deskripsi</td>
+                                    <td>:</td>
+                                    <td class="item-desc"></td>
+                                </tr>
+                                <tr>
+                                    <td>File</td>
+                                    <td>:</td>
+                                    <td><a class="item-file fa fa-download text-primary"></a></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-transition btn-outline-warning" onclick="closeForm()">Tutup</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!--JADWAL-->
+    <div class="tab-pane" id="tab-jadwal" role="tabpanel">
+        <p>Jadwal</p>
+    </div>
+    <div class="tab-pane" id="tab-penerapan" role="tabpanel">
+        <p>Penerapan</p>
+    </div>
+</div>
+<div class="d-none">
+    <ul>
+        <li class="list-group-item" id="template-pemenuhan">
+            <div class="row">
+                <div class="col-sm-5 title">
+                    Pasal
+                </div>
+                <div class="col-sm-7">
+                    <div class="progress">
                     </div>
                 </div>
             </div>
-            <!--JADWAL-->
-            <div class="tab-pane" id="tab-jadwal" role="tabpanel">
-                <p>Jadwal</p>
-            </div>
-            <div class="tab-pane" id="tab-penerapan" role="tabpanel">
-                <p>Penerapan</p>
-            </div>
-        </div>
+        </li>
+    </ul>
+</div>
 <script>
-    var data= JSON.parse('<?php echo json_encode($data)?>');
+    var data = JSON.parse('<?php echo json_encode($data) ?>');
     console.log(data);
     var pasal = $('.list-pasal');
-    for (var p, i = 0; i < pasal.length; i++) {
-        p = pasal[i];
+    for (var p, i = 0; i < data.length; i++) {
+        p = pasal[i]
         $('#' + $(p).children('.parent').text()).children('ul').append(p);
     }
 //    console.log($('#root li'));
-    function form1(item) {
+    function form1(i) {
+        var d = data[i];
         closeForm();
-        showCrud('#formDetail');
-        $('#form-title').text($.trim($(item).parent().children('.title').text()));
-        $('.item-id').val($(item).parent().attr('id'));
-        console.log($(item).parent().children('.desc'));
-        $('.item-desc').val($(item).parent().children('.desc').val());
-        $('#treeview-list').removeClass('col-sm-12');
-        $('#treeview-list').addClass('col-sm-6');
-        $('#treeview-crud').show();
+        showCrud('#form1');
+        $('.item-name').text(d.name);
+        $('.item-desc').text(d.description);
+        if (d.file != null) {
+            $('.item-file').attr('href', '<?php echo base_url('upload/form1/') ?>' + d.file);
+            $('.item-file').removeClass('d-none');
+        } else {
+            $('.item-file').addClass('d-none');
+        }
     }
     function closeForm() {
         $('#treeview-list').removeClass('col-sm-6');
@@ -118,4 +142,26 @@ $role = $this->session->userdata['user']['role'];
         $('#treeview-crud').show();
         $(item).removeClass('d-none');
     }
+    $('#treeview-list li .title').each(function (index) {
+//                PEMENUHAN
+        var r = Math.floor(Math.random() * 101);
+        var clone = $('#template-pemenuhan').clone();
+        clone.attr('id', 'pemenuhan-' + index);
+        clone.find('.title').text($(this).text());
+        var bar = '<div class="progress-bar bg-success" role="progressbar" aria-valuenow="' + r + '" aria-valuemin="0" aria-valuemax="100" style="width: ' + r + '%">' + r + '%</div>';
+        clone.find('.progress').append(bar);
+        $('#tab-pemenuhan .list-group').append(clone);
+        var clone2 = clone.clone();
+//                    JADWAL
+        clone2.attr('id', 'jadwal-' + index);
+        clone2.find('.progress').addClass('jadwal');
+        clone2.find('.progress').removeClass('progress');
+        clone2.find('.jadwal').text('00:00');
+        $('#tab-jadwal').append(clone2);
+//                    PENERAPAN
+        var clone3 = clone2.clone();
+        clone3.attr('id', 'penerapan-' + index);
+        clone3.find('.jadwal').text('-');
+        $('#tab-penerapan').append(clone3);
+    });
 </script>
