dc48b823d59c07dfddf69fdbe875982756f09dfc
kaafi <ma.kaafi@yahoo.co.id>
2020-12-03 16:39:49 +0700

    fix access CRUD

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -26,6 +26,7 @@ class MY_Controller extends CI_Controller {
         $this->data['module'] = $this->module;
         $this->data['activeModule'] = $this->activeModule;
         $this->data['subTitle'] = $this->subTitle;
+        if($view=='edit')$this->access= 'update';
         empty($this->access)?$this->access=$view:null;
         if (empty($this->activeModule['acc_'.$this->access])) {
             $this->data['view'] = 'template/no_access';
@@ -64,6 +65,7 @@ class MY_Controller extends CI_Controller {
             redirect($this->module);
         }
         $this->subTitle = 'Delete';
+        $this->access = 'delete';
         $this->render('template/delete', FALSE);
     }
 
