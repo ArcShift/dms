a57cc92868a132631760842d5500c0061fe72cfb
kaafi <ma.kaafi@yahoo.co.id>
2021-01-20 08:32:33 +0700

    revisi layout perusahaan & standar

--- application/views/dashboard/read.php
+++ application/views/dashboard/read.php
@@ -1,6 +1,6 @@
 <!--<div class="row">
     <div class="col-md-12">
-        <img width="100%" class="rounded" align="right" src="<?php // echo base_url('assets/images/dms-header.jpg')                    ?>" alt="no picture">
+        <img width="100%" class="rounded" align="right" src="<?php // echo base_url('assets/images/dms-header.jpg')                           ?>" alt="no picture">
     </div>
 </div>
 <br/>-->
@@ -21,37 +21,8 @@
             </select>
         </div>
     </div>
+    <canvas id="myChart" height="100"></canvas>
 </div>
-<div id="container" class="card-body">
-
-    <?php // print_r($company) ?>
-    <!--TAB-->
-    <ul class="nav nav-tabs">
-        <?php foreach ($company as $k => $c) { ?>
-            <li class="nav-item"><a data-toggle="tab" href="#tab-c<?= $c['id'] ?>" class="nav-link"><?= $c['name'] ?></a></li>
-        <?php } ?>
-    </ul>
-    <div class="tab-content">
-        <?php foreach ($company as $k => $c) { ?>
-            <div class="tab-pane" id="tab-c<?= $c['id'] ?>" role="tabpanel">
-                <!--aa-->
-                <ul class="nav nav-tabs">
-                    <?php foreach ($c['standard'] as $k2 => $s) { ?>
-                        <li class="nav-item"><a data-toggle="tab" href="#tab-s<?= $s['id'] ?>" class="nav-link" onclick="getGrafik(<?= $c['id'] . ',' . $s['id_standard'] ?>)"><?= $s['name'] ?></a></li>
-                    <?php } ?>
-                </ul>
-                <div class="tab-content">
-                    <?php foreach ($c['standard'] as $k => $s) { ?>
-                        <div class="tab-pane" id="tab-s<?= $s['id_standard'] ?>" role="tabpanel">
-                        </div>
-                    <?php } ?>
-                </div>
-                <!--aa-->
-            </div>
-        <?php } ?>
-    </div>
-</div>
-<canvas id="myChart" height="100"></canvas>
 <div class="row">
     <?php foreach ($box as $key => $b) { ?>
         <?php if (empty($this->session->user['id_company']) | $b['company'] == 'Y') { ?>
@@ -59,7 +30,7 @@
                 <div class="card mb-3 widget-content bg-<?= $b['color'] ?>">
                     <div class="widget-content-wrapper text-white">
                         <!--                    <div>
-                                                <i class="fa fa-<?php // $b['icon']                                       ?>"></i>&nbsp;
+                                                <i class="fa fa-<?php // $b['icon']                                              ?>"></i>&nbsp;
                                             </div>-->
                         <div class="widget-content-left">
                             <div class="widget-heading"><?= $b['title'] ?></div>
@@ -81,82 +52,95 @@
             $('#selectStandar').empty();
             $('#selectStandar').append('<option>~ pilih standar ~</option>');
             for (var i = 0; i < data.length; i++) {
-                $('#selectStandar').append('<option ' + data[i].id + '>' + data[i].name + '</option>');
+                $('#selectStandar').append('<option value="' + data[i].id + '">' + data[i].name + '</option>');
             }
+            $('#selectStandar').change();
         });
     });
-    function getGrafik(company, standard) {
-        $.getJSON('<?= site_url('dashboard/grafik') ?>', {company: company, standard: standard}, function (data) {
-            console.log(data);
-            chart.data.labels = [];
-            chart.data.datasets.data = [];
+    $('#selectStandar').change(function () {
+        $.getJSON('<?= site_url('dashboard/grafik') ?>', {company: $('#selectCompany').val(), standard: $(this).val()}, function (data) {
+            pasal = [];
             for (var i = 0; i < data.length; i++) {
-                if (data[i].parent == null) {
-                    chart.data.labels.push(data[i].name);
-//                    chart.data.datasets.data.push(p.percent);
-//                    getPercent(i);
+                var d = data[i];
+                d.indexChild = [];
+                for (var j = 0; j < data.length; j++) {//get child
+                    var d2 = data[j];
+                    if (d.id == d2.parent) {
+                        d.indexChild.push(j);
+                    }
+                }
+                pasal.push(d);
+            }
+            var labels = [];
+            var gData = [];
+            for (var i = 0; i < data.length; i++) {
+                var d = data[i];
+                if (d.parent == null) {
+                    labels.push(d.name);
+//                    chart.data.labels.push(d.name);
+                    getPercent(i);
+//                    chart.data.datasets.data = [];
+                    gData.push(pasal[i].percent);
+//                    chart.data.datasets.forEach((dataset) => {
+//                        dataset.data.push(pasal[i].percent);
+//                    });
+//                    chart.data.datasets.data.push(50);
+//                    chart.data.datasets.data.push(pasal[i].percent);
                 }
             }
-            chart.update();
+            newGraph(labels, gData);
+//            chart.update();
+        });
+    });
+    var pasal = []
+    function getPercent(index) {
+        var p = pasal[index];
+        p.percent = 0;
+        if (p.indexChild.length == 0) {
+            if (p.doc > 0) {
+                p.percent = 100;
+            }
+        } else {
+            for (var i = 0; i < p.indexChild.length; i++) {
+                var pc = p.indexChild[i];
+                getPercent(pc);
+                p.percent += pasal[pc].percent;
+            }
+            p.percent = Math.round(p.percent / p.indexChild.length);
+        }
+        pasal[index] = p;
+    }
+    var ctx = document.getElementById('myChart');
+    function newGraph(labels, data) {
+        var chart = new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: labels,
+                datasets: [{
+                        label: 'Dokumen',
+                        data: data,
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+            },
+            options: {
+                scale: {
+                    angleLines: {
+                        display: false
+                    },
+                    ticks: {
+                        suggestedMin: 0,
+                        suggestedMax: 100
+                    }
+                }
+            }
         });
     }
-//    var pasal = <?php // echo $grafik          ?>;
-//    var labelData = [];
-//    var docData = [];
-//    for (var i = 0; i < pasal.length; i++) {
-//        var p = pasal[i];
-//        if (p.parent == null) {
-//            getPercent(i);
-//            labelData.push(p.name);
-//            docData.push(p.percent);
-//        }
-//    }
-//    function getPercent(index) {
-//        var p = pasal[index];
-//        p.percent = 0;
-//        if (p.indexChild.length == 0) {
-//            if (p.doc > 0) {
-//                p.percent = 100;
-//            }
-//        } else {
-//            for (var i = 0; i < p.indexChild.length; i++) {
-//                var pc = p.indexChild[i];
-//                getPercent(pc);
-//                p.percent += pasal[pc].percent;
-//            }
-//            p.percent = Math.round(p.percent / p.indexChild.length);
-//        }
-//        pasal[index] = p;
-//    }
-    var ctx = document.getElementById('myChart');
-    var chart = new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: [],
-            datasets: [{
-                    label: 'Dokumen',
-                    data: [],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-        },
-        options: {
-            scale: {
-                angleLines: {
-                    display: false
-                },
-                ticks: {
-                    suggestedMin: 50,
-                    suggestedMax: 100
-                }
-            }
-        }
-    });
 </script>
--- application/views/treeview_detail/read.php
+++ application/views/treeview_detail/read.php
@@ -21,22 +21,23 @@ $role = $this->session->userdata['user']['role'];
         width: 75px;
     }
 </style>
-<div class="main-card mb-3 card">
+<div class="main-card mb-3 card">   
     <div class="card-body">
-        <div class="form-group group-select-perusahaan">
-            <label>Perusahaan</label>
-            <select id="perusahaan" class="form-control" name="perusahaan" required="">
+        <div class="row">
+            <div class="col-sm-6"></div>
+            <div class="col-sm-3">
+                <select id="perusahaan" class="form-control" name="perusahaan" required="">
                 <?php if ($role == 'admin') { ?>
-                    <option value="">-- Perusahaan --</option>
+                    <option value="">~ Pilih Perusahaan ~</option>
                 <?php } ?>
                 <?php foreach ($company as $c) { ?>
                     <option value="<?php echo $c['id'] ?>" <?php echo $c['id'] == $this->input->post('role') ? 'selected' : ''; ?>><?php echo $c['name'] ?></option>
                 <?php } ?>
             </select>
-        </div>
-        <div class="form-group">
-            <label>Standar</label>
-            <select id="standar" class="form-control" name="perusahaan" required=""></select>
+            </div>
+            <div class="col-sm-3">
+                <select id="standar" class="form-control" name="perusahaan" required=""></select>
+            </div>
         </div>
         <div class="main-card mb-3 card">
             <div id="container" class="card-body">
@@ -812,7 +813,7 @@ $role = $this->session->userdata['user']['role'];
             $.post('<?php echo site_url($module); ?>/standard', {'id': $(this).val()}, function (data) {
                 var d = JSON.parse(data);
                 $('#standar').html('');
-                $('#standar').append('<option value="">-- Standar --</option>');
+                $('#standar').append('<option value="">~ Pilih Standar ~</option>');
                 for (var i = 0; i < d.length; i++) {
                     $('#standar').append('<option value="' + d[i].id + '">' + d[i].name + '</option>');
                 }
