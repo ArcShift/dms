a2ae4370dd861bb4c606de0e5f1bc9f2046888b7
kaafi <ma.kaafi@yahoo.co.id>
2021-01-27 14:18:54 +0700

    revisi layout

--- application/views/dashboard/detail.php
+++ application/views/dashboard/detail.php
@@ -114,7 +114,7 @@ if (empty($this->session->activeCompany)) {
         </div>
         <div class="row">
             <div class="col-md-12 col-lg-6">
-                <div class="mb-3 card">
+                <div class="mb-3 card" style="height: 560px">
                     <div class="card-header-tab card-header-tab-animation card-header">
                         <div class="card-header-title">
                             <i class="header-icon lnr-apartment icon-gradient bg-love-kiss"> </i>
@@ -221,7 +221,7 @@ if (empty($this->session->activeCompany)) {
                 </div>
             </div>
             <div class="col-md-12 col-lg-6">
-                <div class="mb-3 card">
+                <div class="mb-3 card" style="height: 560px">
                     <div class="card-header-tab card-header">
                         <div class="card-header-title">
                             <i class="header-icon lnr-rocket icon-gradient bg-tempting-azure"> </i>
@@ -538,11 +538,11 @@ if (empty($this->session->activeCompany)) {
                 options: {}
             });
             var dataTableConfig = {
-                "order": [],
                 "ordering": false,
                 "searching": false,
                 "info": false,
-                "lengthChange": false
+                "lengthChange": false,
+                "pageLength": 8,
             }
             $('#tableDistribusi').DataTable(dataTableConfig);
         </script>
