d3dcb3daddae1f2638307804df5bf5f1e1a26e1c
kaafi <ma.kaafi@yahoo.co.id>
2021-04-14 14:21:23 +0700

    fix text

--- application/views/perbaikan_gap_analisa/index.php
+++ application/views/perbaikan_gap_analisa/index.php
@@ -70,7 +70,7 @@ $unit = [];
         <div class="modal-content">
             <form method="post" enctype="multipart/form-data">
                 <div class="modal-header">
-                    <h5 class="modal-title">Edit Hasil Gap Analisa</h5>
+                    <h5 class="modal-title">Edit Perbaikan Gap Analisa</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
