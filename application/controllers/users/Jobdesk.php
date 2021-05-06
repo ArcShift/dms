a6727006945f3d7ebbeffa3d191fde67539f532e
kaafi <ma.kaafi@yahoo.co.id>
2021-05-06 09:33:12 +0700

    jobdesk

--- application/controllers/users/Jobdesk.php
+++ application/controllers/users/Jobdesk.php
@@ -1,20 +1,17 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * Description of Jobdesk
- *
- * @author kaafi
- */
 class Jobdesk extends MY_User {
 
     function index() {
+        $this->db->select('uk.*');
+        $this->db->join('position_personil pp', 'pp.id_unit_kerja = uk.id AND pp.id_personil=' . $this->session->user['id_personil']);
+        $this->data['unit_kerja'] = $this->db->get('unit_kerja uk')->result();
         $this->render('jobdesk');
+    }
+
+    function get_data() {
+        $data = $this->db->get_where('jobdesk',['id_unit_kerja'=>$this->input->get('id')])->result();
+        echo json_encode($data);
     }
 
 }
--- application/views/users/jobdesk.php
+++ application/views/users/jobdesk.php
@@ -1,1 +1,41 @@
-JOBDESK
+<div class="card">
+    <div class="card-header">
+        <div class="col-sm-9"></div>
+        <div class="col-sm-3">
+            <select class="form-control" id="selectUK">
+                <?php foreach ($unit_kerja as $k => $uk) { ?>
+                    <option value="<?= $uk->id ?>"><?= $uk->name ?></option>
+                <?php } ?>
+            </select>
+        </div>
+    </div>
+    <div class="card-body">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Jobdesk</th>
+                    <th>Jobdesk Saya</th>
+                </tr>
+            </thead>
+            <tbody id="tbMain">
+
+            </tbody>
+        </table>
+    </div>
+</div>
+<script>
+    $(document).ready(function () {
+        $('#selectUK').change();
+    })
+    $('#selectUK').change(function () {
+        $.getJSON('jobdesk/get_data', {id: $(this).val()}, function (data) {
+            $('#tbMain').empty();
+            for (var d of data) {
+                $('#tbMain').append('<tr>'
+                        + '<td>' + d.name + '</td>'
+                        + '<td>-</td>'
+                        + '</tr>');
+            }
+        });
+    });
+</script>
