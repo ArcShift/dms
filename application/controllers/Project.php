ddfb3f366e7daefc250e34916f1da9a9245b4ee6
kaafi <ma.kaafi@yahoo.co.id>
2021-06-16 16:56:13 +0700

    fungsi hapus

--- application/controllers/Project.php
+++ application/controllers/Project.php
@@ -6,15 +6,20 @@ class Project extends MY_Controller {
 
     function index() {
         if($this->input->post('create')){
-            print_r($this->input->post());
             $this->db->set('id_company', $this->session->activeCompany['id']);
             $this->db->set('nama', $this->input->post('name'));
             $this->db->set('deskripsi', $this->input->post('desc'));
             $this->db->insert('project');
+        }elseif($this->input->post('hapus')){
+            $this->db->where('id', $this->input->post('id'));
+            $this->db->delete('project');
         }
         $this->data['menuStandard']= 'company';
         $this->subModule = 'read';
-        $this->data['data'] = $this->db->get_where('project', ['id_company'=>$this->session->activeCompany['id']])->result();
+        $this->db->select('p.*, COUNT(t.id) AS tugas');
+        $this->db->join('tugas t', 't.id_project = p.id', 'LEFT');
+        $this->db->group_by('p.id');
+        $this->data['data'] = $this->db->get_where('project p', ['p.id_company'=>$this->session->activeCompany['id']])->result();
         $this->render('index');
     }
 
--- application/views/project/index.php
+++ application/views/project/index.php
@@ -7,27 +7,34 @@
                     <th>Proyek</th>
                     <th>Deskripsi</th>
                     <th>Tugas</th>
+                    <th>Aksi</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($data as $d) { ?>
+                <?php foreach ($data as $k => $d) { ?>
                     <tr>
                         <td><?= $d->nama ?></td>
                         <td><?= $d->deskripsi ?></td>
-                        <td><span class="badge badge-danger">0</span></td>
+                        <td><span class="badge badge-<?= $d->tugas == 0 ? 'danger' : 'primary' ?>"><?= $d->tugas ?></span></td>
+                        <td>
+                            <button class="btn btn-sm btn-outline-primary fa fa-edit" onclick="initEdit(<?= $k ?>)"></button>
+                            <?php if ($d->tugas == 0) { ?>
+                                <button class="btn btn-sm btn-outline-danger fa fa-trash" onclick="initHapus(<?= $k ?>)"></button>
+                            <?php } ?>
+                        </td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
     </div>
 </div>
-<!--MODAL DELETE TUGAS-->
+<!--MODAL CREATE-->
 <div class="modal fade" id="modalCreate">
     <div class="modal-dialog" role="document">
         <form method="post">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title">Buat Proyek Baru</h5>
+                    <h5 class="modal-title"></h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -35,11 +42,11 @@
                 <div class="modal-body">
                     <div class="form-group">
                         <label><b>Nama</b></label>
-                        <input class="form-control" name="name" required="">
+                        <input class="form-control input-nama" name="name" required="">
                     </div>
                     <div class="form-group">
                         <label><b>Deskripsi</b></label>
-                        <textarea class="form-control" name="desc"></textarea>
+                        <textarea class="form-control input-desc" name="desc"></textarea>
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -50,9 +57,54 @@
         </form>
     </div>
 </div>
+<!--MODAL DELETE-->
+<div class="modal fade" id="modalHapus">
+    <div class="modal-dialog" role="document">
+        <form method="post">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Hapus Proyek</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label><b>Nama</b></label>
+                        <input class="form-control input-nama" disabled="">
+                        <input class="form-control input-id" name="id" required="" hidden="">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-outline-primary" data-dismiss="modal">Batal</button>
+                    <button class="btn btn-outline-danger" name="hapus" value="ok">Hapus</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
 <script>
+    var data = <?= json_encode($data) ?>;
     $('#createProject').click(function () {
         var m = $('#modalCreate');
         m.modal('show');
+        m.find('.modal-title').html('Buat Proyek Baru');
+        m.find('form').trigger('reset');
     });
+    function initHapus(idx) {
+        var d = data[idx];
+        var m = $('#modalHapus');
+        m.modal('show');
+        m.find('.input-nama').val(d.nama);
+        m.find('.input-id').val(d.id);
+    }
+    function initEdit(idx) {
+        var m = $('#modalCreate');
+        var d = data[idx];
+        m.modal('show');
+        m.find('.modal-title').html('Edit Proyek');
+        m.find('.input-nama').val(d.nama);
+        m.find('.input-id').val(d.id);
+        m.find('.input-desc').val(d.deskripsi);
+    }
 </script>
